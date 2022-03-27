55ae94267d4927ca3c85ce11d35bcda2b079567c
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-03-27 13:13:41 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -406,8 +406,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
         buttons = [[
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
         ], [
-            InlineKeyboardButton('👥 DEVS', url='https://t.me/Ls_Supportz'),
-            InlineKeyboardButton('🤖 Support', url='https://t.me/Ls_Supportz')
+            InlineKeyboardButton('👥 DEV', url='https://t.me/mr_MKN'),
+            InlineKeyboardButton('🤖 Support', url='https://t.me/mkn_bots_updates')
         ], [
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
             InlineKeyboardButton('😊 About', callback_data='about')
@@ -438,8 +438,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         )
     elif query.data == "about":
         buttons = [[
-            InlineKeyboardButton('🤖 Support', url='https://t.me/Ls_Supportz'),
-            InlineKeyboardButton('♥️ Source', url='https://github.com/Akbotmakersnew/LUCIFER')
+            InlineKeyboardButton('🤖 Support', url='https://t.me/mkn_bots_updates')
         ], [
             InlineKeyboardButton('🏠 Home', callback_data='start'),
             InlineKeyboardButton('🔐 Close', callback_data='close_data')
