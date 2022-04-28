f564c538f8f5c5ea8fecaa815d1b68e32dc26b47
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-04-28 12:49:28 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/MrMKN/Thorappan-v3.git /EvaMaria
+  git clone https://github.com/MrMKN/Thorappan-v3.git /Thorappan-v3   
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /Thorappan-v3
 fi
-cd /EvaMaria
+cd /Thorappan-v3
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
