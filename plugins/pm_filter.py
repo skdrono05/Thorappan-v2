550b67755199f7282b52c3cfe40f9a0a450d00ed
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-04-29 12:36:08 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -394,77 +394,84 @@ async def cb_handler(client: Client, query: CallbackQuery):
             alert = alert.replace("\\n", "\n").replace("\\t", "\t")
             await query.answer(alert, show_alert=True)
     if query.data.startswith("file"):
-        ident, file_id = query.data.split("#")
-        files_ = await get_file_details(file_id)
-        if not files_:
-            return await query.answer('No such file exist.')
-        files = files_[0]
-        title = files.file_name
-        size = get_size(files.file_size)
-        type = files.file_type
-        mention = query.from_user.mention
-        f_caption = files.caption
-        settings = await get_settings(query.message.chat.id)
-        if CUSTOM_FILE_CAPTION:
-            try:
-                f_caption = CUSTOM_FILE_CAPTION.format(file_name='' if title is None else title,
-                                                       file_size='' if size is None else size,
-                                                       file_caption='' if f_caption is None else f_caption)
-
-                buttons = [[
-                  InlineKeyboardButton('🔰 Main Group 🔰', url='https://t.me/mknmovies')
-                  ]]                                        
-            except Exception as e:
-                logger.exception(e)
-            f_caption = f_caption
-            size = size
-            mention = mention
-        if f_caption is None:
-            f_caption = f"{files.file_name}"
-            size = f"{files.file_size}"
-            mention = f"{query.from_user.mention}"
-
+        clicked = query.from_user.id
         try:
-            if AUTH_CHANNEL and not await is_subscribed(client, query):
-                await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
-                return
-            elif settings['botpm']:
-                await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
-                return
-            else:
-                ms = await client.send_cached_media(
-                    chat_id=CH_FILTER,
-                    file_id=file_id,
-                    caption=f'<b>Hey 👋 {query.from_user.mention}</b>\n<b>File Name:</b>🔘 size - {title}\n<b>↬ Size:</b> {size}\n𝘛𝘩𝘪𝘴 𝘔𝘦𝘴𝘴𝘢𝘨𝘦 𝘸𝘪𝘭𝘭 𝘣𝘦 𝘈𝘶𝘵𝘰-𝘥𝘦𝘭𝘦𝘵𝘦𝘥 𝘢𝘧𝘵𝘦𝘳 5 𝘔𝘪𝘯𝘶𝘵𝘦𝘴 𝘵𝘰 𝘈𝘷𝘰𝘪𝘥 𝘊𝘰𝘱𝘺𝘳𝘪𝘨𝘩𝘵 𝘐𝘴𝘴𝘶𝘦𝘴 & 𝘋𝘰𝘯𝘵 𝘧𝘰𝘳𝘨𝘦𝘵 𝘵𝘰 𝘍𝘰𝘳𝘸𝘢𝘳𝘥 𝘵𝘩𝘦 𝘧𝘪𝘭𝘦 𝘵𝘰 𝘚𝘢𝘷𝘦𝘥 𝘔𝘦𝘴𝘴𝘢𝘨𝘦𝘴 𝘣𝘦𝘧𝘰𝘳𝘦 𝘋𝘦𝘭𝘦𝘵𝘦.!\n\nRequested Group - <b>{query.message.chat.title}</b>\n\n<i>\n=========== • ✠ • ===========\n▫️ ɢʀᴏᴜᴘ : @mknmovies \n▫️ ᴄʜᴀɴɴᴇʟ : @Mr_Movies_Main\n=========== • ✠ • ============</i>',
-                    protect_content=True if ident == "filep" else False 
-                )
-                msg1 = await query.message.reply(
-                f'<b>Hey 👋{query.from_user.mention}\n'
-                f'<b>📫 Yᴏuʀ Fɪʟᴇ ɪꜱ Rᴇᴀᴅʏ 👇\n\n'
-                f'<b>↬ ғɪʟᴇ ɴᴀᴍᴇ: </b>{title}\n'
-                f'<b>↬ sɪᴢᴇ:</b> {size}\n\n'
-                '𝘛𝘩𝘪𝘴 𝘍𝘪𝘭𝘦 𝘸𝘪𝘭𝘭 𝘣𝘦 𝘋𝘦𝘭𝘦𝘵𝘦𝘥 𝘪𝘯 5 𝘔𝘪𝘯𝘶𝘵𝘦𝘴.!\nതാഴെ ഉള്ള click to download അമർത്തി പിന്നീട് വരുന്ന\nചാനലിൽ നിന്ന് file saved മെസ്സേജിലേക്ക് മാറ്റി download ചെയ്യുക \n⚠️ copyright കാരണം മൂവി 5mint ൽ delet ആകും ⚠️!',
-                True,
-                'html',
-                reply_markup=InlineKeyboardMarkup(
-                    [
+            typed = query.message.reply_to_message.from_user.id
+        except:
+            typed = query.from_user.id
+            pass
+        if int(clicked) == typed:
+            ident, file_id = query.data.split("#")
+            files_ = await get_file_details(file_id)
+            if not files_:
+                return await query.answer('No such file exist.')
+            files = files_[0]
+            title = files.file_name
+            size = get_size(files.file_size)
+            type = files.file_type
+            mention = query.from_user.mention
+            f_caption = files.caption
+            settings = await get_settings(query.message.chat.id)
+            if CUSTOM_FILE_CAPTION:
+                try:
+                    f_caption = CUSTOM_FILE_CAPTION.format(file_name='' if title is None else title,
+                                                           file_size='' if size is None else size,
+                                                           file_caption='' if f_caption is None else f_caption)
+
+                    buttons = [[
+                      InlineKeyboardButton('🔰 Main Group 🔰', url='https://t.me/mknmovies')
+                      ]]                                        
+                except Exception as e:
+                    logger.exception(e)
+                f_caption = f_caption
+                size = size
+                mention = mention
+            if f_caption is None:
+                f_caption = f"{files.file_name}"
+                size = f"{files.file_size}"
+                mention = f"{query.from_user.mention}"
+
+            try:
+                if AUTH_CHANNEL and not await is_subscribed(client, query):
+                    await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
+                    return
+                elif settings['botpm']:
+                    await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
+                    return
+                else:
+                    ms = await client.send_cached_media(
+                        chat_id=CH_FILTER,
+                        file_id=file_id,
+                        caption=f'<b>Hey 👋 {query.from_user.mention}</b>\n<b>File Name:</b>🔘 size - {title}\n<b>↬ Size:</b> {size}\n𝘛𝘩𝘪𝘴 𝘔𝘦𝘴𝘴𝘢𝘨𝘦 𝘸𝘪𝘭𝘭 𝘣𝘦 𝘈𝘶𝘵𝘰-𝘥𝘦𝘭𝘦𝘵𝘦𝘥 𝘢𝘧𝘵𝘦𝘳 5 𝘔𝘪𝘯𝘶𝘵𝘦𝘴 𝘵𝘰 𝘈𝘷𝘰𝘪𝘥 𝘊𝘰𝘱𝘺𝘳𝘪𝘨𝘩𝘵 𝘐𝘴𝘴𝘶𝘦𝘴 & 𝘋𝘰𝘯𝘵 𝘧𝘰𝘳𝘨𝘦𝘵 𝘵𝘰 𝘍𝘰𝘳𝘸𝘢𝘳𝘥 𝘵𝘩𝘦 𝘧𝘪𝘭𝘦 𝘵𝘰 𝘚𝘢𝘷𝘦𝘥 𝘔𝘦𝘴𝘴𝘢𝘨𝘦𝘴 𝘣𝘦𝘧𝘰𝘳𝘦 𝘋𝘦𝘭𝘦𝘵𝘦.!\n\nRequested Group - <b>{query.message.chat.title}</b>\n\n<i>\n=========== • ✠ • ===========\n▫️ ɢʀᴏᴜᴘ : @mknmovies \n▫️ ᴄʜᴀɴɴᴇʟ : @Mr_Movies_Main\n=========== • ✠ • ============</i>',
+                        protect_content=True if ident == "filep" else False 
+                    )
+                    msg1 = await query.message.reply(
+                    f'<b>Hey 👋{query.from_user.mention}\n'
+                    f'<b>📫 Yᴏuʀ Fɪʟᴇ ɪꜱ Rᴇᴀᴅʏ 👇\n\n'
+                    f'<b>↬ ғɪʟᴇ ɴᴀᴍᴇ: </b>{title}\n'
+                    f'<b>↬ sɪᴢᴇ:</b> {size}\n\n'
+                    '𝘛𝘩𝘪𝘴 𝘍𝘪𝘭𝘦 𝘸𝘪𝘭𝘭 𝘣𝘦 𝘋𝘦𝘭𝘦𝘵𝘦𝘥 𝘪𝘯 5 𝘔𝘪𝘯𝘶𝘵𝘦𝘴.!\nതാഴെ ഉള്ള click to download അമർത്തി പിന്നീട് വരുന്ന\nചാനലിൽ നിന്ന് file saved മെസ്സേജിലേക്ക് മാറ്റി download ചെയ്യുക \n⚠️ copyright കാരണം മൂവി 5mint ൽ delet ആകും ⚠️!',
+                    True,
+                    'html',
+                    reply_markup=InlineKeyboardMarkup(
                         [
-                            InlineKeyboardButton("📥 𝐂𝐥𝐢𝐜𝐤 𝐇𝐞𝐫𝐞 𝐭𝐨 𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝 𝐭𝐡𝐞 𝐅𝐢𝐥𝐞 📥", url = ms.link)
-                        ],
-                        [
-                            InlineKeyboardButton("⚠️ 𝐂𝐚𝐧'𝐭 𝐕𝐢𝐞𝐰 𝐓𝐡𝐞 𝐅𝐢𝐥𝐞 ❓ 𝐂𝐥𝐢𝐜𝐤 𝐇𝐞𝐫𝐞 ⚠️", url = f"{CH_LINK}")
+                            [
+                                InlineKeyboardButton("📥 𝐂𝐥𝐢𝐜𝐤 𝐇𝐞𝐫𝐞 𝐭𝐨 𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝 𝐭𝐡𝐞 𝐅𝐢𝐥𝐞 📥", url = ms.link)
+                            ],
+                            [
+                                InlineKeyboardButton("⚠️ 𝐂𝐚𝐧'𝐭 𝐕𝐢𝐞𝐰 𝐓𝐡𝐞 𝐅𝐢𝐥𝐞 ❓ 𝐂𝐥𝐢𝐜𝐤 𝐇𝐞𝐫𝐞 ⚠️", url = f"{CH_LINK}")
+                            ]
                         ]
-                    ]
+                    )
                 )
-            )
-            await query.answer('Check Out The Chat',show_alert=False)
-            await asyncio.sleep(300)
-            await msg1.delete()
-            await msg.delete()
-            del msg1, msg
-        except Exception as e:
-            logger.exception(e, exc_info=True)
-            await query.answer(f"Encountering Issues", True)
+                await query.answer('Check Out The Chat',show_alert=False)
+                await asyncio.sleep(300)
+                await msg1.delete()
+                await msg.delete()
+                del msg1, msg
+            except Exception as e:
+                logger.exception(e, exc_info=True)
+                await query.answer(f"Encountering Issues", True)
         else:
             return await query.answer(f"⚠️ 𝙃𝙚𝙮, {query.from_user.first_name}! 𝙏𝙝𝙖𝙩'𝙨 𝙉𝙤𝙩 𝙁𝙤𝙧 𝙔𝙤𝙪. 𝙋𝙡𝙚𝙖𝙨𝙚 𝙍𝙚𝙦𝙪𝙚𝙨𝙩 𝙔𝙤𝙪𝙧 𝙊𝙬𝙣. ഇത് നിങ്ങൾ search ചെയ്തതല്ല നിങ്ങൾക് വേണ്ടത് നിങ്ങൾ search ചെയ്യൂ 🙏.", show_alert=True)
            
