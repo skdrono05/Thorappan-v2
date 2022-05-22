dbd67d20147b34146af6e69642eb967d9132ef96
Movie Show <105733634+skdrono05@users.noreply.github.com>
2022-05-22 07:11:57 +0530

    Create Main.py

---
+++ http:/Main.py
@@ -0,0 +1,5 @@
+))],
+
+                     [InlineKeyboardButton(text="👥 Support Group", url="https://t.me/+qzQY0WCu8884YWZl"), InlineKeyboardButton(text="🔔 Update Channel", url="https://t.me/Drono_Bot_Updates")],
+
+                     [InlineKeyboardButton(text="OWNER", url="https://t.me/drono05"), InlineKeyboardButton(text="🛠 Help", url="https://t.me/{}?start=help".format(bot.username)) ]]))
